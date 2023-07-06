b5a516b1b5ee3b09ef21efe9e9f2123eaaa842a9
KarthickAravind <karthickaraivndofficial@gmail.com>
2023-07-06 22:05:20 +0530

    Updated PAGINATION for Navigation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// https://github.com/VoloshchenkoAl/revealer
+document.addEventListener('DOMContentLoaded', function() {
+    var dots = document.querySelectorAll('.pagination input[type="radio"]');
+    var sections = document.querySelectorAll('section');
+  
+    for (var i = 0; i < dots.length; i++) {
+      dots[i].addEventListener('change', function() {
+        var checkedDot = document.querySelector('.pagination input[type="radio"]:checked');
+        var targetSectionId = checkedDot.getAttribute('id').replace('dot-', 'section-');
+        var targetSection = document.getElementById(targetSectionId);
+        targetSection.scrollIntoView({
+          behavior: 'smooth'
+        });
+      });
+    }
+  });
+  
